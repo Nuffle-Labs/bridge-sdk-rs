542c21874b286e62d9e9420f44a1cd967084fac3
Ivan Frolov <59515280+frolvanya@users.noreply.github.com>
2024-11-04 18:59:37 +0100

    fix: typo in sign_transfer args (#44)

--- bridge-sdk/connectors/omni-connector/src/omni_connector.rs
+++ bridge-sdk/connectors/omni-connector/src/omni_connector.rs
@@ -1,5 +1,5 @@
-use bridge_connector_common::result::{BridgeSdkError, Result};
 use borsh::BorshSerialize;
+use bridge_connector_common::result::{BridgeSdkError, Result};
 use ethers::{abi::Address, prelude::*};
 use near_contract_standards::storage_management::StorageBalance;
 use near_crypto::SecretKey;
@@ -363,7 +363,7 @@ impl OmniConnector {
     pub async fn sign_transfer(
         &self,
         origin_nonce: u128,
-        fee_recepient: Option<AccountId>,
+        fee_recipient: Option<AccountId>,
         fee: Option<Fee>,
     ) -> Result<FinalExecutionOutcomeView> {
         let near_endpoint = self.near_endpoint()?;
@@ -375,7 +375,7 @@ impl OmniConnector {
             "sign_transfer".to_string(),
             serde_json::json!({
                 "nonce": origin_nonce.to_string(),
-                "fee_recepient": fee_recepient,
+                "fee_recipient": fee_recipient,
                 "fee": fee,
             })
             .to_string()
