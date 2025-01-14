0742379055277a83856de23c7b1cba90a297c2b3
Ivan Frolov <59515280+frolvanya@users.noreply.github.com>
2025-01-13 20:19:06 -0500

    chore: base fee multiplied by 2 (#70)

--- bridge-sdk/bridge-clients/evm-bridge-client/src/evm_bridge_client.rs
+++ bridge-sdk/bridge-clients/evm-bridge-client/src/evm_bridge_client.rs
@@ -335,7 +335,7 @@ impl EvmBridgeClient {
         };
 
         tx.max_priority_fee_per_gas = Some(max_priority_fee_per_gas);
-        tx.max_fee_per_gas = Some(base_fee_per_gas);
+        tx.max_fee_per_gas = Some(base_fee_per_gas * 2);
 
         Ok(())
     }
