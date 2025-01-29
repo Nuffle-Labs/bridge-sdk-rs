50c58f978dd552a630ebd465fa5e9501fe3bd6d0
Ivan Frolov <59515280+frolvanya@users.noreply.github.com>
2025-01-29 15:32:17 -0500

    chore: updated defaults for testnet (#87)
    * chore: updated eth bridge address
    
    * chore: updated rpc endpoints

--- bridge-cli/src/defaults.rs
+++ bridge-cli/src/defaults.rs
@@ -36,19 +36,19 @@ pub const NEAR_TOKEN_LOCKER_ID_TESTNET: &str = "omni-locker.testnet";
 pub const NEAR_LIGHT_CLIENT_ETH_ADDRESS_TESTNET: &str =
     "0x202cdf10bfa45a3d2190901373edd864f071d707";
 
-pub const ETH_RPC_TESTNET: &str = "https://ethereum-sepolia.blockpi.network/v1/rpc/public";
+pub const ETH_RPC_TESTNET: &str = "https://ethereum-sepolia-rpc.publicnode.com";
 pub const ETH_CHAIN_ID_TESTNET: u64 = 11_155_111;
 pub const ETH_BRIDGE_TOKEN_FACTORY_ADDRESS_TESTNET: &str =
-    "0xa9108f7F83Fb661e611991116D526fCa1a9585ab";
+    "0x3701B9859Dbb9a4333A3dd933ab18e9011ddf2C8";
 pub const ETH_CONNECTOR_ACCOUNT_ID_TESTNET: &str = "aurora";
 pub const ETH_CUSTODIAN_ADDRESS_TESTNET: &str = "0xe0320b199863D7f6D5bBa62741aACB312110DEf7";
 
-pub const BASE_RPC_TESTNET: &str = "https://base-sepolia.blockpi.network/v1/rpc/public";
+pub const BASE_RPC_TESTNET: &str = "https://base-sepolia-rpc.publicnode.com";
 pub const BASE_CHAIN_ID_TESTNET: u64 = 84_532;
 pub const BASE_BRIDGE_TOKEN_FACTORY_ADDRESS_TESTNET: &str =
     "0x0C981337fFe39a555d3A40dbb32f21aD0eF33FFA";
 
-pub const ARB_RPC_TESTNET: &str = "https://arbitrum-sepolia.blockpi.network/v1/rpc/public";
+pub const ARB_RPC_TESTNET: &str = "https://arbitrum-sepolia-rpc.publicnode.com";
 pub const ARB_CHAIN_ID_TESTNET: u64 = 421_614;
 pub const ARB_BRIDGE_TOKEN_FACTORY_ADDRESS_TESTNET: &str =
     "0xd025b38762B4A4E36F0Cde483b86CB13ea00D989";
@@ -61,3 +61,4 @@ pub const FAST_BRIDGE_ACCOUNT_ID_TESTNET: &str = "fastbridge.testnet";
 pub const FAST_BRIDGE_ADDRESS_TESTNET: &str = "0x0B2C4871C9bAD795746C05c1539A8B1f26c26357";
 
 pub const WORMHOLE_API_TESTNET: &str = "https://api.testnet.wormholescan.io";
+
