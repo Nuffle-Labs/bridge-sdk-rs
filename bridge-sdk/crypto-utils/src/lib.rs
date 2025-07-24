3d54c08298afd8801c9c008da791bb2d9a181f99
Hesham <hesham@nuff.tech>
2025-07-24 11:19:04 +0200

    feat: add mainnet support to address derivation with network-specific MPC keys

--- bridge-sdk/crypto-utils/src/lib.rs
+++ bridge-sdk/crypto-utils/src/lib.rs
@@ -6,9 +6,14 @@ use near_primitives::types::AccountId;
 use std::str::FromStr;
 
 const MPC_KEY: &str = "secp256k1:4NfTiv3UsGahebgTaHyD9vF8KYKMBnfd6kh94mK6xv8fGBiJB8TBtFMP5WWXz6B89Ac1fbpzPwAvoyQebemHFwx3";
+const MPC_KEY_MAINNET: &str = "secp256k1:3tFRbMqmoa6AAALMrEFAYCEoHcqKxeW38YptwowBVBtXK1vo36HDbUWuR6EZmoK4JcH6HDkNMGGqP1ouV7VZUWya";
 
-pub fn derive_address(near_account_id: &AccountId, path: &str) -> [u8; 64] {
-    let mpc_key = PublicKey::from_str(MPC_KEY).unwrap();
+pub fn derive_address(near_account_id: &AccountId, path: &str, mainnet: bool) -> [u8; 64] {
+    let mpc_key = if mainnet {
+        PublicKey::from_str(MPC_KEY_MAINNET).unwrap()
+    } else {
+        PublicKey::from_str(MPC_KEY).unwrap()
+    };
 
     let mut bytes = vec![0x04];
     bytes.extend(mpc_key.key_data());
